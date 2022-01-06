3cfcb92bdac12e4b03652dc773573c05d83ea9aa
Isna Yousuf <isnayousuf77@gmail.com>
2022-01-06 11:01:52 +0500

    change variablesjoke.js

--- joke.js
+++ joke.js
@@ -5,45 +5,26 @@ const jokes = document.querySelector('#joke');
 const jokeBtn = document.querySelector('#jokeBtn');
 
 
-// function getJokes() {
-//     const setHeader = {
-//         headers : {
-//             Accept: "application/json"    
-//         }
-//     }
-//     fetch('https://icanhazdadjoke.com/', setHeader)
-//     .then ( (response)=> response.json())
-//     .then ( (data)=> {
-//         jokes.innerHTML = data.joke;    
-//      })
-//     .catch ( ( error)=> {
-//         console.console(error)          
-//     })
-// }
-// jokeBtn.addEventListener('click', getJokes);
-// getJokes();    
-
-
-async function getJokes() {
-   
-    try{
-       
+function getJokes() {
     const setHeader = {
         headers : {
             Accept: "application/json"    
         }
     }
-     const response = await fetch('https://icanhazdadjoke.com/', setHeader)
-    const data = await response.json()
-
-    jokes.innerHTML = data.joke; 
-    }
-catch(error){
-    console.log(error)
-
+    fetch('https://icanhazdadjoke.com/', setHeader)
+    .then ( (response)=> response.json())
+    .then ( (data)=> {
+        jokes.innerHTML = data.joke;    
+     })
+    .catch ( ( error)=> {
+        console.console(error)          
+    })
 }
+jokeBtn.addEventListener('click', getJokes);
+getJokes();    
 
-}
+
+
 jokeBtn.addEventListener('click', getJokes);
 getJokes();    
 
